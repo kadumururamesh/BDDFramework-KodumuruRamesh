6b7f300b8945f485275f6d3e42aaa41743aa323b
kadumururamesh <161864615+kadumururamesh@users.noreply.github.com>
2024-03-18 00:41:25 +0530

    import testng.annotation from userutils class

--- src/main/java/com/orange/Utils/UserActions.java
+++ src/main/java/com/orange/Utils/UserActions.java
@@ -5,8 +5,7 @@ import java.net.URL;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+
 import com.orange.Utils.BaseClass;
 
 
