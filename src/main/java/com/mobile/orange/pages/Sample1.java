2a43b4012c5d5289090cba3db606465b213e6b85
kadumururamesh <161864615+kadumururamesh@users.noreply.github.com>
2024-03-22 00:50:08 +0530

    Chrome browser code

--- src/main/java/com/mobile/orange/pages/Sample1.java
+++ src/main/java/com/mobile/orange/pages/Sample1.java
@@ -30,27 +30,39 @@ public class Sample1 extends IntializeAppiumServer{
 		IntializeAppiumServer.stopAppiumServer();
 	}
 	@Test
-	void test() {
+	void test() throws InterruptedException {
+		ChromiumDriver driver1=null;
 		UiAutomator2Options options=new UiAutomator2Options();
-		options.setCapability("platfromName", "android");
-		options.setCapability("device", "Device13");
-		options.setCapability("platformVersion","13.0");
-		options.setCapability("automationName","uiautomator2");
-		options.setCapability("appPackage","io.appium.android.apis");
-		options.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
-		options.setCapability("noReset", true);
+		//options.setCapability("platfromName", "android");
+		//options.setCapability("device", "Device13");
+		//options.setCapability("platformVersion","13.0");
+		//options.setCapability("automationName","uiautomator2");
+		//options.setCapability("appPackage","io.appium.android.apis");
+		//options.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+		//options.setCapability("noReset", true);
 		//
-		
+		options.setCapability("platformName", "android");
+		options.setCapability("device", "Device13");
+		options.setCapability("platformVersion", "13.0.0");
+		options.setCapability("automationname", "uiautomator2");
+		options.setCapability("browserName", "Chrome");
+		//options.setCapability("fullReset", true);
 		
 		//
 		
 		
 		try {
-			ChromiumDriver driver1=new ChromiumDriver(new URL("http://127.0.0.1:4723/"), options);
+			driver1=new ChromiumDriver(new URL("http://127.0.0.1:4723/"), options);		
+			
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		//Thread.sleep(20000);
+		//driver1.get("https://www.google.co.in/");
+		driver1.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		
 		
 		
 	}
